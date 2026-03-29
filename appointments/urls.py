0f9366ab5ac121c42329bcecafa102db324557c5
sifatpHero1 <lamborg803@gmail.com>
2026-03-29 14:02:46 +0600

    doctor appointment

--- appointments/urls.py
+++ appointments/urls.py
@@ -5,7 +5,7 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(next_page='login'), name='logout'),
+    path('logout/', auth_views.LogoutView.as_view(next_page='login', http_method_names=['get', 'post']), name='logout'),
     path('register/', views.register, name='register'),
     path('doctors/', views.doctor_list, name='doctor_list'),
     path('book/<int:doctor_id>/', views.book_appointment, name='book_appointment'),
